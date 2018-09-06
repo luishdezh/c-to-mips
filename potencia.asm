4a8cd04dde702be2b4fcd4c0f0ca69dedfe4a2af
luishdezh <is703108@iteso.mx>
2018-09-06 11:52:52 -0500

    Potencia terminada

--- potencia.asm
+++ potencia.asm
@@ -7,8 +7,8 @@
 .text
 
 main:	
-	addiu $s0, $zero, 21 # m = 6
-	addiu $s1, $zero, 5 # n = 6
+	addiu $s0, $zero, 6 # m = 6
+	addiu $s1, $zero, 6 # n = 6
 	jal potencia
 	j exit
 	
@@ -16,17 +16,17 @@ potencia:
 
 	slti $t0, $s1, 1 #  if n > 1 jump to loop
 	beq $t0, $zero, loop
-	addi $v0, $zero, 1 
+	addi $v0, $zero, 1 # result = 1
 	jr $ra 
 loop:
 
-	addi $sp, $sp, -8 # decrease stack pointer
-	sw $ra, 4($sp) #save return adress
+	addi $sp, $sp, -4 # decrease stack pointer
+	sw $ra, 0($sp) #save return address
 	addi $s1, $s1, -1 #decrease n
 	jal potencia
-	lw $ra, 4($sp)
-	addi $sp, $sp, 8
-	mul $v0, $s0, $v0
+	lw $ra, 0($sp) #load return address
+	addi $sp, $sp, 4 #increase stack pointer
+	mul $v0, $s0, $v0 # result = result x m
  	jr $ra
  	
 exit: 
