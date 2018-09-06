7cb348b22d302852534db1f660a56041fd8338cb
luishdezh <is703108@iteso.mx>
2018-09-06 15:07:58 -0500

    Array Sum Final

--- arraysum.asm
+++ arraysum.asm
@@ -9,9 +9,9 @@
 .text
 
 	addiu $s0, $zero, 10 # i = 9
-	lui $at, 0x1001
+	lui $at, 0x1001 # cargar en $s1 la direccion base del arreglo
 	ori $s1, $at, 0
-	addiu $s2, $zero,0
+	addiu $s2, $zero,0 #contador
 	jal sum
 	j exit
 
@@ -25,13 +25,13 @@ loop:
 
 	addi $sp, $sp, -4 # decrease stack pointer
 	sw $ra, 0($sp) #save return address
-	addi $s2, $s2, 1
+	addi $s2, $s2, 1 # incrementar contador
 	jal sum
 	lw $ra, 0($sp) #load return address
 	addi $sp, $sp, 4 #increase stack pointer
-	lw $t1,($s1)
-	addi $s1, $s1, 4
-	add $v0, $v0, $t1
+	lw $t1,($s1) #cargar en t1 el valor de la direccion que contenga s1
+	addi $s1, $s1, 4 #aumentar en 4 s1
+	add $v0, $v0, $t1 # sumar elementos
  	jr $ra
 exit:
 
