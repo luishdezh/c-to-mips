b6970a54bb3200c128a9549c12b01a3441dc2aaf
luishdezh <is703108@iteso.mx>
2018-09-06 13:04:44 -0500

    Producto terminado

--- producto.asm
+++ producto.asm
@@ -9,14 +9,19 @@
 	
 .text
 
-	addiu $t0, $zero, 0
-	addiu $s0, $zero, 0
+	addiu $t0, $zero, 0 # i
+	addiu $s0, $zero, 0 # result
 
-	la $s1, Vector_1 # Loading an address
-	
-	la $s2, Vector_2  # Loading an address
+loop:
 
-producto:
+	lw $t1,Vector_1($t0) # Cargar en t1 el valor del arreglor 1 en la posicion [i]
+	lw $t2,Vector_2($t0) # Cargar en t2 el valor del arreglor 2 en la posicion [i]
+	mult $t1, $t2 #multiplicacion que se guarda en lo
+	mflo $t4 #guardar lo que hay en lo en $t4
+	addi $t0, $t0, 4 # i = i + 4
+	add $s0, $s0, $t4 # resultado = resultado + $t4
+	slti $t4, $t0, 36 #if i < 36 jump to loop
+	bne $t4, $zero, loop
 	
 
 	
